c8e9f903572413213961d45d4e7ad66a07b79beb
chead <chead@localhost>
2010-01-02 11:07:18 +0000

    Simplify into an FSM.

--- electronics/VHDL/Chris/common/xbeebytetransmitter.vhd
+++ electronics/VHDL/Chris/common/xbeebytetransmitter.vhd
@@ -8,7 +8,7 @@ entity XBeeByteTransmitter is
 		Data : in std_ulogic_vector(7 downto 0);
 		Load : in std_ulogic;
 		SOP : in std_ulogic;
-		Busy : out std_ulogic := '0';
+		Busy : out std_ulogic;
 
 		SerialData : out std_ulogic_vector(7 downto 0);
 		SerialLoad : out std_ulogic := '0';
@@ -17,54 +17,43 @@ entity XBeeByteTransmitter is
 end entity XBeeByteTransmitter;
 
 architecture Behavioural of XBeeByteTransmitter is
-	signal HoldBusy : boolean := false;
-	signal EscapedData : std_ulogic_vector(7 downto 0) := X"00";
-	signal EscapeSent : boolean := false;
+	signal NeedsEscape : boolean;
+	type StateType is (Idle, SendingData, SendingEscape);
+	signal State : StateType := Idle;
+	signal DataL : std_ulogic_vector(7 downto 0);
 begin
+	NeedsEscape <= (Data = X"7E") or (Data = X"7D") or (Data = X"11") or (Data = X"13");
+	Busy <= '1' when (Load = '1') or (SOP = '1') or (State /= Idle) else '0';
+
 	process(Clock)
 	begin
 		if rising_edge(Clock) then
-			if SOP = '1' then
-				Busy <= '1';
-				SerialData <= X"7E";
-				SerialLoad <= '1';
-				HoldBusy <= true;
-				EscapeSent <= false;
-			elsif Load = '1' then
-				Busy <= '1';
-				SerialLoad <= '1';
-				HoldBusy <= true;
-				if Data = X"7E" or Data = X"7D" or Data = X"11" or Data = X"13" then
-					SerialData <= X"7D";
-					EscapedData <= Data xor X"20";
-					EscapeSent <= true;
-				else
-					SerialData <= Data;
-					EscapeSent <= false;
-				end if;
-			elsif SerialBusy = '1' then
-				Busy <= '1';
-				SerialData <= Data;
-				SerialLoad <= '0';
-				HoldBusy <= true;
-			else
-				SerialData <= EscapedData;
-				if HoldBusy then
-					Busy <= '1';
-					SerialLoad <= '0';
-					HoldBusy <= false;
-				elsif EscapeSent then
-					Busy <= '1';
+			SerialLoad <= '0';
+
+			if SerialBusy = '0' then
+				if State = Idle then
+					if SOP = '1' then
+						SerialData <= X"7E";
+						SerialLoad <= '1';
+						State <= SendingData;
+					elsif Load = '1' and NeedsEscape then
+						SerialData <= X"7D";
+						SerialLoad <= '1';
+						State <= SendingEscape;
+						DataL <= Data;
+					elsif Load = '1' and not NeedsEscape then
+						SerialData <= Data;
+						SerialLoad <= '1';
+						State <= SendingData;
+					end if;
+				elsif State = SendingData then
+					State <= Idle;
+				elsif State = SendingEscape then
+					SerialData <= DataL xor X"20";
 					SerialLoad <= '1';
-					HoldBusy <= true;
-					EscapeSent <= false;
-				else
-					Busy <= '0';
-					SerialLoad <= '0';
-					HoldBusy <= false;
-					EscapeSent <= false;
+					State <= SendingData;
 				end if;
-			end if; 
+			end if;
 		end if;
 	end process;
 end architecture Behavioural;
