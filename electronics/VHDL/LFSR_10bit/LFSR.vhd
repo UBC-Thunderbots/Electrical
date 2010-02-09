6837a05b1e7bd8a29a17efa8a826617173090f14
binaryblade <binaryblade@localhost>
2010-02-09 18:25:04 +0000

    Sample LFSR for system identification

---
+++ electronics/VHDL/LFSR_10bit/LFSR.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity LFSR_10bit is
+    Port ( Reset	 : in STD_LOGIC;
+	   CLK		: in STD_LOGIC;
+	   Value_out : out  STD_LOGIC
+	);	  
+end LFSR_10bit;
+
+architecture Behavioral of LFSR_10bit is
+begin
+
+	count_process: process(Reset,CLK)
+		variable State : STD_LOGIC_VECTOR(9 downto 0) := "1111111111";
+	begin
+		if rising_edge(CLK) then
+			if Reset = '1' then
+				State := "1111111111";
+			else
+				State := (State(3) XOR State(0))&State(9 downto 1);
+			end if;
+		end if;
+		
+		Value_out <= State(0);
+	end process;
+end Behavioral;
+
---
+++ electronics/VHDL/LFSR_10bit/LFSRTest.vhd
@@ -0,0 +1,63 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.all;
+USE ieee.numeric_std.ALL;
+use ieee.std_logic_arith.all;
+
+ENTITY LFSRTest IS
+END LFSRTest;
+ 
+ARCHITECTURE behavior OF LFSRTest IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT LFSR_10bit
+    Port ( Reset	 : in STD_LOGIC;
+	   CLK		: in STD_LOGIC;
+	   Value_out : out  STD_LOGIC);
+    END COMPONENT;
+    
+   constant CLK_period : time := 5 ms;
+
+   --Inputs
+   signal Reset : std_logic := '0';
+   signal CLK : std_logic;
+ 
+	--Outputs
+   	signal Value_out : std_logic;
+
+BEGIN
+
+	-- Instantiate the Unit Under Test (UUT)
+   uut: LFSR_10bit PORT MAP (
+          Reset => Reset,
+          CLK => CLK,
+          Value_out => Value_out
+        );
+ 
+	
+	
+	
+ 
+   clock_process :process
+   begin
+	for i in 0 to 5000 loop
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;	 
+	end loop;
+	wait;
+ end process;
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin	
+   		reset <= '1';
+   		wait for CLK_period*10;
+   		reset <= '0';
+		wait;
+   end process;
+
+END;
