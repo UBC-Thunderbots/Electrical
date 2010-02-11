0ea46f289878df0b38ad60d4b8d10d43395298fb
chead <chead@localhost>
2010-02-11 23:54:47 +0000

    Remove unused unisim include.

--- electronics/VHDL/Chris/synth/steptest.vhd
+++ electronics/VHDL/Chris/synth/steptest.vhd
@@ -2,9 +2,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-library unisim;
-use unisim.vcomponents.all;
-
 entity StepTest is
 	port(
 		-- The 50MHz canned oscillator.
