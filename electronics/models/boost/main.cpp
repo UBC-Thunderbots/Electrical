c10858c04bf52ff7ed913061536e8db5ae377d29
binaryblade <binaryblade@localhost>
2010-07-10 23:42:08 +0000

    added a flyback model

--- electronics/models/boost/main.cpp
+++ electronics/models/boost/main.cpp
@@ -4,19 +4,41 @@
 #include <iostream>
 #include <fstream>
 
+#define BOOST
 
-const double Vin = 14.8;
+#ifdef FLYBACK
+const double Vin = 12;
 const double Vout_max = 240;
+const double transformer_ratio = 10;
 const double Vd = 0.7;
-const double L=47e-6;
+const double L=5e-6;
 const double C=5.4e-3;
-const double R=34e-3;
-const double f=25e3;
-const double Ipk_target = 6;
-const double Dmax= Ipk_target * L * f / Vin;
-const unsigned int iters = 100;
-const double charge_time = 5;
+const double Rpri = 7.7e-3;
+const double Rsec = 515e-3;
+const double f=68e3;
+const double Ipk = 6;
 const double sim_time = 10;
+const double D_P = 100;
+const double D_margin = 0.90;
+const unsigned int pwm_levels = 1024;
+#endif
+
+#ifdef BOOST 
+const double Vin = 17;
+const double Vout_max = 240;
+const double transformer_ratio = 1;
+const double Vd = 0.7;
+const double L=22e-6;
+const double C=5.4e-3;
+const double Rpri = 14.6e-3;
+const double Rsec = 14.6e-3;
+const double f=68e3;
+const double sim_time = 10;
+const double D_P = 100;
+const double D_margin = 0.90;
+const double Ipk = 10;
+const unsigned int pwm_levels = 128;
+#endif
 
 double I_start;
 double Voltage_start;
@@ -42,49 +64,79 @@ int main(int argc, char* argv[]) {
 
 	I.push_back(0);
 	Iav.push_back(0);
+
+	#ifdef BOOST
 	Vout.push_back(Vin);
+	#else
+	Vout.push_back(0);
+	#endif
+
 	I_max.push_back(0);
 	time.push_back(0);
+	Dlist.push_back(0);
 
 	unsigned int loops = floor(f*sim_time);	
 	
-	Vsp.push_back(Vin);
-	double increase = (Vout_max-Vin)/charge_time/f;
-	for(unsigned int index = 0;index< loops;++index) {
-			if(index/f < charge_time) {
-				Vsp.push_back(*(Vsp.end()-1)+increase);
-			} else {
-				Vsp.push_back(*(Vsp.end()-1));
-			}
-	}
 	
 
 	double D;
+	double D_1;
+	double D_2;
+	bool changed = false;
 	double I_av;
 	double decrease_rate;
 	double increase_rate;
-
-	for(std::vector<double>::iterator voltage_sp = Vsp.begin(); voltage_sp != Vsp.end();++voltage_sp) {
+	for(unsigned int index =0; index < loops ; ++index) {
 		I_start = *(I.end()-1);
 		Voltage_start = *(Vout.end()-1);
 
-		//D = 1 - Vin / *voltage_sp;
-		D = (*voltage_sp - Voltage_start)/240*100;
+		increase_rate = (Vin - I_start*Rpri)/L;
+		increase_rate = (increase_rate < 0)?0:increase_rate;
 
-		D = (D > Dmax)?Dmax:D;
+		decrease_rate =  (Vin*transformer_ratio - Voltage_start - I_start/transformer_ratio*Rsec- Vd)/transformer_ratio/L;
+		decrease_rate = (decrease_rate > 0)?0:decrease_rate;
+		
+		D_1 = decrease_rate/(decrease_rate - increase_rate)*D_margin;
+
+		D_2 = (Vout_max - Voltage_start)/Vout_max*D_P;	
+		D_2 = floor(D_2*pwm_levels)/pwm_levels;
+
+		if(D_1 < 0.5/pwm_levels) {
+			D_1 = Ipk * f * L / Vin;	
+		}
+
+		D_1 = floor(D_1*pwm_levels)/pwm_levels;
+		
+		if(D_2 < D_1 && !changed) {
+			changed = true;
+			std::cout << "Transition Voltage: " << Voltage_start << std::endl;
+			std::cout << "Duty: " << D_2 << std::endl;
+		}
+
+		if(changed) {
+			D = D_2;
+		} else {
+			D = D_1;
+		}
+
+		D = (D > 1)?1:D;
 		D = (D<0)?0:D;
-		increase_rate = (Vin - I_start*R)/L;
-		decrease_rate =  (Vin - Voltage_start - I_start*R - Vd)/L;
+	
+		if(I_start > 0.1)
+			D=0;
+
+		Dlist.push_back(D);
 		
 		Imax = I_start + D/f*increase_rate;
 
 		I_end = Imax + decrease_rate*(1-D)/f;	
+
 		if(I_end < 0) {
-			Voltage_end = Voltage_start + 0.5*Imax/C * Imax/fabs(decrease_rate);
+			Voltage_end = Voltage_start + 0.5*Imax/transformer_ratio/C * Imax/fabs(decrease_rate);
 			I_av = (Imax + I_start) / 2 * D + Imax/2 * Imax/fabs(decrease_rate) * f; 
 			I_end = 0;
 		} else {
-			Voltage_end = Voltage_start + (1-D)/f/C * (1.5*I_end + 0.5*Imax);
+			Voltage_end = Voltage_start + (1-D)/f/C * (1.5*I_end + 0.5*Imax)/transformer_ratio;
 			I_av = (I_start + Imax)/2 * D + (Imax + I_end)/2 *(1-D);
 		}	
 		Iav.push_back(I_av);
@@ -97,7 +149,7 @@ int main(int argc, char* argv[]) {
 	
 
 	for(unsigned int index = 0; index < I.size(); ++index) {
-		file << time[index] << "," << I[index] << "," << Iav[index] << "," << I_max[index] << "," << Vsp[index] << "," << Vout[index] << std::endl;
+		file << time[index] << "," << I[index] << "," << Iav[index] << "," << I_max[index] << "," << Vout[index] << "," << Dlist[index] << std::endl;
 	}
 		file.close();
 	return 0;
