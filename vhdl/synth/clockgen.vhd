5f292acd5f43ecd0dd27fc33986178f2c122affb
chead <chead@localhost>
2011-01-11 07:51:01 +0000

    Basic toplevel for testing new boards.

--- vhdl/synth/clockgen.vhd
+++ vhdl/synth/clockgen.vhd
@@ -7,113 +7,79 @@ use unisim.vcomponents.all;
 
 entity ClockGen is
 	port(
-		PICOscillator : in std_ulogic;
-		Clock100 : out std_ulogic;
-		Clock10 : out std_ulogic;
-		Clock1 : out std_ulogic
-	);
+		Oscillator : in std_ulogic;
+		Reset : in std_ulogic;
+		Locked : out std_ulogic;
+		Clock500k : out std_ulogic;
+		Clock8M : out std_ulogic;
+		Clock256M : out std_ulogic);
 end entity ClockGen;
 
 --
--- This entity generates clocks for the FPGA from an input 2MHz clock provided
--- by the PIC. The buffers and DCMs are arranged in the following layout:
+-- This entity generates clocks for the FPGA from an input 8MHz clock provided by the packaged oscillator.
+-- The buffers and DCMs are arranged in the following layout:
 --
 --
---                                                     /------------------------------------------\
---                                                     |                                          |
---                                                     |  /------------------------\              |
---                                                     |  |                        |  10MHz   |\  |
---                                                     \--| CLKFB             CLK0 |----------| +-+--[]
---                                                        |                        |          |/
---                 /-----------------------\              |                        |
---    2MHz   |\    |                       |  10MHz  |\   |                        |  100MHz  |\
--- []--------| +---| CLKIN      CLKFX (5×) |---------| +--| CLKIN      CLKFX (10×) |----------| +----[]
---           |/    |                       |         |/   |                        |          |/
---         IBufG   |                       |              |                        |
---                 |                LOCKED |--\   |\      |                        |   1MHz   |\
---                 |                       |   \--| O-----| RST        CLKDV (÷10) |----------| +----[]
---                 \-----------------------/      |/      |                        |          |/
---                                                        \------------------------/
+--     /------------------------------------------\
+--     |                                          |
+--     |  /------------------------\              |
+--     |  |                        |  8MHz    |\  |
+--     \--| CLKFB             CLK0 |----------| +-+--[]
+--        |                        |          |/
+--        |                        |
+--   |\   |                        |  256Mhz  |\
+-- --| +--| CLKIN      CLKFX (32×) |----------| +----[]
+--   |/   |                        |          |/
+--        |                        |
+--        |                        |  500kHz  |\
+-- -------| RST        CLKDV (÷32) |----------| +----[]
+--        |                        |          |/
+--        \------------------------/
 --
 architecture Behavioural of ClockGen is
-	signal PICOscillatorBuffered : std_ulogic;
-
-	signal DCM1Locked : std_ulogic;
-	signal DCM1Clock10 : std_ulogic;
-	signal DCM1Clock10Buffered : std_ulogic;
-
-	signal DCM2Clock1 : std_ulogic;
-	signal DCM2Clock1Buffered : std_ulogic;
-	signal DCM2Clock10 : std_ulogic;
-	signal DCM2Clock10Buffered : std_ulogic;
-	signal DCM2Clock100 : std_ulogic;
-	signal DCM2Clock100Buffered : std_ulogic;
+	signal OscillatorBuffered : std_ulogic;
+	signal DCMClock0 : std_ulogic;
+	signal DCMClock0Buffered : std_ulogic;
+	signal DCMClockDV : std_ulogic;
+	signal DCMClockFX : std_ulogic;
 begin
 	IBufferG : IBufG
 	port map(
-		I => PICOscillator,
-		O => PICOscillatorBuffered
+		I => Oscillator,
+		O => OscillatorBuffered
 	);
 
-	DCM1 : DCM_SP
+	DCM : DCM_SP
 	generic map(
-		CLKIN_PERIOD => 500.0,
-		CLK_FEEDBACK => "NONE",
-		CLKFX_MULTIPLY => 5,
+		CLKIN_PERIOD => 125.0,
+		CLKDV_DIVIDE => 16.0,
+		CLKFX_MULTIPLY => 32,
 		CLKFX_DIVIDE => 1,
-		STARTUP_WAIT => true
-	)
+		STARTUP_WAIT => false)
 	port map(
-		CLKIN => PICOscillatorBuffered,
-		RST => '0',
+		CLKIN => OscillatorBuffered,
+		CLKFB => DCMClock0Buffered,
+		RST => Reset,
 		PSEN => '0',
-		CLKFX => DCM1Clock10,
-		LOCKED => DCM1Locked
-	);
+		CLK0 => DCMClock0,
+		CLKDV => DCMClockDV,
+		CLKFX => DCMClockFX,
+		LOCKED => Locked);
 
-	DCM2 : DCM_SP
-	generic map(
-		CLKIN_PERIOD => 100.0,
-		CLKDV_DIVIDE => 10.0,
-		CLKFX_MULTIPLY => 10,
-		CLKFX_DIVIDE => 1,
-		STARTUP_WAIT => true
-	)
+	BufG0 : BufG
 	port map(
-		CLKIN => DCM1Clock10Buffered,
-		CLKFB => DCM2Clock10Buffered,
-		RST => not DCM1Locked,
-		PSEN => '0',
-		CLK0 => DCM2Clock10,
-		CLKDV => DCM2Clock1,
-		CLKFX => DCM2Clock100
-	);
+		I => DCMClock0,
+		O => DCMClock0Buffered);
 
-	BufG1 : BufG
+	BufGDV : BufG
 	port map(
-		I => DCM1Clock10,
-		O => DCM1Clock10Buffered
-	);
+		I => DCMClockDV,
+		O => Clock500k);
 
-	BufG2 : BufG
+	BufGFX : BufG
 	port map(
-		I => DCM2Clock1,
-		O => DCM2Clock1Buffered
-	);
+		I => DCMClockFX,
+		O => Clock256M);
 
-	BufG3 : BufG
-	port map(
-		I => DCM2Clock10,
-		O => DCM2Clock10Buffered
-	);
-
-	BufG4 : BufG
-	port map(
-		I => DCM2Clock100,
-		O => DCM2Clock100Buffered
-	);
-
-	Clock1 <= DCM2Clock1Buffered;
-	Clock10 <= DCM2Clock10Buffered;
-	Clock100 <= DCM2Clock100Buffered;
+	Clock8M <= DCMClock0Buffered;
 end architecture Behavioural;
