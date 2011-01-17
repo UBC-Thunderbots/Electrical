03020d9b888cd0067054b48a778f603db9079bda
chead <chead@localhost>
2011-01-17 02:30:45 +0000

    Clean up encoder handling.

--- vhdl/common/graycounter.vhd
+++ vhdl/common/graycounter.vhd
@@ -1,62 +1,67 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+
+library work;
 
 entity GrayCounter is
-	generic (
-		Width : positive;
-		Sign : integer range -1 to 1
-	);
-	port (
-		Clock1 : in std_ulogic;
-
-		A : in std_ulogic;
-		B : in std_ulogic;
-
-		Reset : in std_ulogic;
-		Count : out signed(Width - 1 downto 0)
-	);
+	generic(
+		Max : positive);
+	port(
+		Clock : in std_ulogic;
+		Reset : in boolean;
+		Input : in work.types.encoder;
+		Strobe : in boolean;
+		Value : out integer range -Max to Max);
 end entity GrayCounter;
 
 architecture Behavioural of GrayCounter is
-	constant CountUp : signed(Width - 1 downto 0) := to_signed(Sign, Width);
-	constant CountDown : signed(Width - 1 downto 0) := to_signed(-Sign, Width);
-	signal OldA : std_ulogic := '0';
-	signal OldB : std_ulogic := '0';
-	signal CountBuf : signed(Width - 1 downto 0) := to_signed(0, Width);
 begin
-	Count <= CountBuf;
+	process(Clock)
+		type DeltaType is (NONE, ADD, SUB);
 
-	process(Clock1)
-		variable Delta : signed(Width - 1 downto 0);
+		variable Accumulator : integer range -Max to Max;
+		variable OldInput : work.types.encoder;
+
+		variable Delta : DeltaType;
 	begin
-		if rising_edge(Clock1) then
-			if OldA = '0' and OldB = '0' and A = '1' and B = '0' then 
-				Delta := CountUp;
-			elsif OldA = '1' and OldB = '0' and A = '1' and B = '1' then 
-				Delta := CountUp;
-			elsif OldA = '1' and OldB = '1' and A = '0' and B = '1' then 
-				Delta := CountUp;
-			elsif OldA = '0' and OldB = '1' and A = '0' and B = '0' then 
-				Delta := CountUp;
-			elsif OldA = '0' and OldB = '0' and A = '0' and B = '1' then 
-				Delta := CountDown;
-			elsif OldA = '0' and OldB = '1' and A = '1' and B = '1' then 
-				Delta := CountDown;
-			elsif OldA = '1' and OldB = '1' and A = '1' and B = '0' then 
-				Delta := CountDown;
-			elsif OldA = '1' and OldB = '0' and A = '0' and B = '0' then 
-				Delta := CountDown;
+		if rising_edge(Clock) then
+			if Reset then
+				Value <= 0;
+				Accumulator := 0;
 			else
-				Delta := to_signed(0, Width);
+				if not OldInput(0) and not OldInput(1) and Input(0) and not Input(1) then 
+					Delta := ADD;
+				elsif OldInput(0) and not OldInput(1) and Input(0) and Input(1) then 
+					Delta := ADD;
+				elsif OldInput(0) and OldInput(1) and not Input(0) and Input(1) then 
+					Delta := ADD;
+				elsif not OldInput(0) and OldInput(1) and not Input(0) and not Input(1) then 
+					Delta := ADD;
+				elsif not OldInput(0) and not OldInput(1) and not Input(0) and Input(1) then 
+					Delta := SUB;
+				elsif not OldInput(0) and OldInput(1) and Input(0) and Input(1) then 
+					Delta := SUB;
+				elsif OldInput(0) and OldInput(1) and Input(0) and not Input(1) then 
+					Delta := SUB;
+				elsif OldInput(0) and not OldInput(1) and not Input(0) and not Input(1) then 
+					Delta := SUB;
+				else
+					Delta := NONE;
+				end if;
+
+				case Delta is
+					when NONE => null;
+					when ADD => Accumulator := Accumulator + 1;
+					when SUB => Accumulator := Accumulator - 1;
+				end case;
+
+				if Strobe then
+					Value <= Accumulator;
+					Accumulator := 0;
+				end if;
 			end if;
-			OldA <= A;
-			OldB <= B;
-			if Reset = '1' then
-				CountBuf <= Delta;
-			else
-				CountBuf <= CountBuf + Delta;
-			end if;
+
+			OldInput := Input;
 		end if;
 	end process;
 end architecture Behavioural;
--- vhdl/common/types.vhd
+++ vhdl/common/types.vhd
@@ -1,5 +1,8 @@
 package types is
 	type hall is array(1 to 3) of boolean;
+
 	type motor_phase is (FLOAT, LOW, HIGH);
 	type motor_phase3 is array(1 to 3) of motor_phase;
+
+	type encoder is array(0 to 1) of boolean;
 end package types;
