818f0850b85b0daefebd92928f020ac1808af80b
timing <timing@localhost>
2015-11-22 01:14:55 +0000

    merging lps_calib branch

--- controls/lps/characterization/open_file.m
+++ controls/lps/characterization/open_file.m
@@ -1,7 +1,12 @@
 clear
 f = fopen('/tmp/thunderbots_lps_pipe','r');
 past = [];
+plotting_data=[];
 max=100;
+samples=10;
+disp_count=0;
+f_hist=figure;
+f_trend=figure;
 while(f>0)
     s = fgetl(f);
 %     if ~ischar(s), break, end
@@ -14,12 +19,34 @@ while(f>0)
         if(size(past,1)>max*4*2)
             past=past(max*4+1:end);
         end
+        disp_count=disp_count+1;
+    end
+    if (disp_count >= samples)
+        
         if(size(current_data,1)<max)
-            plot(current_data(:,1))
+            plotting_data=current_data(:,:)
         else
-            plot(current_data(end-max+1:end,1))
+            plotting_data=current_data(end-max+1:end,:);
         end
+        figure(f_hist)
+        for i=1:4
+            subplot(2,2,i);
+            hist(plotting_data(:,i));
+        end
+        % plot trend data
+        figure(f_trend)
+        % cal
+        trend_data=zeros(size(plotting_data));
+        % first column is total reflection
+        trend_data(:,1)=sum(plotting_data,2);
+        
+        for i=1:4
+            subplot(4,1,i);
+            plot(plotting_data(:,i));
+        end
+        
         pause(.001);
+        disp_count=0;
     end
 end
 
