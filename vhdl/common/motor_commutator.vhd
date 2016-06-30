ede2bf02636a8c4264e6cd0406fa3606a9e82211
chead <chead@localhost>
2016-06-30 14:19:45 +0000

    Use current instead of latched Hall sensor stuck status for deciding whether to drive motor or not.

--- vhdl/common/motor_commutator.vhd
+++ vhdl/common/motor_commutator.vhd
@@ -17,6 +17,7 @@ entity MotorCommutator is
 end entity MotorCommutator;
 
 architecture RTL of MotorCommutator is
+	signal StuckHighNow, StuckLowNow : boolean;
 	signal StuckHighBits, StuckLowBits : boolean_vector(0 to 2);
 	signal Swapped : boolean_vector(0 to 2);
 	signal NPhase : boolean_vector(0 to 2);
@@ -26,8 +27,9 @@ begin
 	-- state (which is an invalid encoding), we consider a stuck sensor to exist.
 	-- We consider the stuck sensor to have cleared once every sensor has been
 	-- observed in the opposite state at least once.
+	StuckHighNow <= Hall(0) and Hall(1) and Hall(2);
+	StuckLowNow <= (not Hall(0)) and (not Hall(1)) and (not Hall(2));
 	process(HostClock) is
-		variable StuckHighNow, StuckLowNow : boolean;
 		variable I : natural range 0 to 2;
 	begin
 		if rising_edge(HostClock) then
@@ -35,9 +37,6 @@ begin
 				StuckHighBits <= (false, false, false);
 				StuckLowBits <= (false, false, false);
 			else
-				StuckHighNow := (Hall(0) and Hall(1) and Hall(2));
-				StuckLowNow := ((not Hall(0)) and (not Hall(1)) and (not Hall(2)));
-
 				for I in 0 to 2 loop
 					StuckHighBits(I) <= (StuckHighBits(I) and Hall(I)) or StuckHighNow;
 					StuckLowBits(I) <= (StuckLowBits(I) and not Hall(I)) or StuckLowNow;
@@ -50,10 +49,10 @@ begin
 
 	Swapped <= Hall when Direction = REVERSE else not Hall;
 
-	process(HallValid, Swapped, StuckHigh, StuckLow) is
+	process(HallValid, Swapped, StuckHighNow, StuckLowNow) is
 	begin
 		for I in Phases'range loop
-			if (not HallValid) or StuckHigh or StuckLow then
+			if (not HallValid) or StuckHighNow or StuckLowNow then
 				Phases(I) <= FLOAT;
 			elsif not Swapped(I) and Swapped((I + 1) mod 3) then
 				Phases(I) <= LOW;
