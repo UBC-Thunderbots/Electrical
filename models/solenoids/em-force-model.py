86e1370a583f28a0939cd8c45ecd82e1c9654339
Anna <annatqwang@gmail.com>
2019-11-01 15:06:23 -0700

    added force calculations and changed inputs to be in terms of time

--- models/solenoids/em-force-model.py
+++ models/solenoids/em-force-model.py
@@ -17,7 +17,7 @@ import math
 # Constants
 
 mu = 4 * math.pi * pow(10, -7)  # permeability constant of free space
-
+e = 2.7182818284590452353602874713526624977572470936999
 
 # Values determined by design, these being the parameters we can change by changing the design of the solenoid
 
@@ -30,28 +30,51 @@ numCoils = 100  # number of coils
 crossSectionAreaSolenoid = pow((innerDiameter / 2 + shellCasingThickness), 2) * math.pi
 # area calculation (cross section of solenoid)
 
+# External Values, changeable, but dependent on other components
+
+maxVoltage = 4  # the voltage across the capacitor
+resistance = 1  # resistance of the device
+capacitance = 3  # capacitance of our capacitor
+
 # input values, these being values we can change during play, when we have finalized design
-current = 25  # ### We want to use time as our input
+
+timeElapsed = 4  # ### we want to change length of pulse
+directionVector = 1
 
 # Preliminary Calculations
+
+timeConstant = resistance*capacitance
+
+maxCurrent = maxVoltage/resistance
+
+current = maxCurrent * pow(e, -timeElapsed/timeConstant)
+
+print (current)
 
 lineCurrentDensity: float = (current / lengthSolenoid) * numCoils  # this is used in the calculation for remanence
 remanence = mu * lineCurrentDensity # remanent flux density (remanence)
 
-constantMultiplier = (mu * numCoils * current) / (2 * lengthSolenoid * (outerDiameter - innerDiameter))
-# this calculation for constantMultiplier is for debugging. It is an arbitrary value and have no meaning on its own
+# The following are used for debugging purposes. They are arbitrary values and have no meaning in isolation
 
+constantMultiplier = (mu * numCoils * current) / (2 * lengthSolenoid * (outerDiameter - innerDiameter))
 
 length_addDistanceFromCenter = lengthSolenoid + 2 * distPToCenter
 
 length_subtractDistanceFromCenter = lengthSolenoid - 2 * distPToCenter
 
-# The following are used for debugging purposes. They are arbitrary values and have no meaning in isolation
 ln_termA = math.log((outerDiameter + math.sqrt(pow(outerDiameter, 2) + pow(length_addDistanceFromCenter, 2))) / (
             innerDiameter + math.sqrt(pow(innerDiameter, 2) + pow(length_addDistanceFromCenter, 2))))
 ln_termB = math.log((outerDiameter + math.sqrt(pow(outerDiameter, 2) + pow(length_subtractDistanceFromCenter, 2))) / (
             innerDiameter + math.sqrt(pow(innerDiameter, 2) + pow(length_subtractDistanceFromCenter, 2))))
 
+# calculating field strength
+
 fieldStrengthAtP = constantMultiplier / length_addDistanceFromCenter * math.log(ln_termA) \
                    + constantMultiplier / length_subtractDistanceFromCenter * math.log(ln_termB)
 print(fieldStrengthAtP)
+
+# calculating force
+
+forceOnPiston = fieldStrengthAtP * remanence * crossSectionAreaSolenoid * directionVector / mu  # r
+
+print(forceOnPiston)
